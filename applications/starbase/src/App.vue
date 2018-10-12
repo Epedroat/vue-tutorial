fb47080068b92c802833901382afc0c8a5463dd3
Bruno Mauricio Willrich Gehlen <bruno.gehlen@cwi.com.br>
2018-10-12 15:06:33 -0300

    creating Character component and using fetch method to get star wars character from api

--- applications/starbase/src/App.vue
+++ applications/starbase/src/App.vue
@@ -1,16 +1,21 @@
 <template>
 <div id="app">
     <h3>{{title}}</h3>
+    <Character :id="2"/>
 </div>
 </template>
 
 <script>
+import Character from './components/Character.vue'
 export default {
     name: 'app',
     data() {
         return {
             title: "Generate your team"
         }
+    },
+    components: {
+        Character
     }
 }
 </script>
---
+++ applications/starbase/src/components/Character.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    {{character}}
+</div>
+</template>
+
+<script>
+export default {
+    props: ['id'],
+    data() {
+        return {
+            character: {
+
+            }
+        }
+    },
+    methods: {
+        fetchCharacter(id) {
+            fetch('https://swapi.co/api/people/'+id, {
+                method: 'GET'
+            })
+                .then(response => response.json())
+                .then(json => this.character = json)
+        }
+    },
+    created() {
+        this.fetchCharacter(this.id)
+    }
+}
+</script>
+
